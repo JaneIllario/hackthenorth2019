dd38549200555c60f700b49cd84a6c1033285a17
ChrisChrisLoLo <sporewoh@gmail.com>
2019-09-14 23:41:23 -0600

    write new controllers to work with the bluetooth controller

--- json_io.py
+++ json_io.py
@@ -7,6 +7,9 @@ import random, json
 
 app = Flask(__name__)
 
+#The throw value that the server holds. Set on BLE input, sent and cleared on VR input.
+throwVal = None
+
 @app.route('/', methods=['GET'])
 def index():  # pragma: no cover
 	return render_template('index.html',name='Joes')
@@ -25,6 +28,18 @@ def pass_login_data():
 		result += str(item['make']) + '\n'
 
 	return result
+
+#Get data from the ble controller. Does not currently work.
+@app.route("/ble", methods = ['POST'])
+def load_ble_data():
+    data = request.get_json()
+    throwVal = data.val
+
+@app.route("/vr", methods = ["GET"])
+def unload_ble_data():
+    result = throwVal
+    throwVal = 0
+    return result
 
 if __name__ == "__main__":
 	app.run()

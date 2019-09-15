647a956a1083838822d8af75d1961c2e491c836f
JaneIllario <illariojane@gmail.com>
2019-09-14 23:41:50 -0400

    update with flask

---
+++ json_io.py
@@ -0,0 +1,30 @@
+#!flask/bin/python
+
+import sys
+
+from flask import Flask, render_template, request, redirect, Response
+import random, json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():  # pragma: no cover
+	return render_template('index.html',name='Joes')
+    # content = get_file('./templates/index.html')
+    # return Response(content, mimetype="text/html")
+
+
+@app.route("/", methods = ['POST'])
+def pass_login_data():
+	# read json + reply
+	data = request.get_json()
+	result = ''
+
+	for item in data:
+		# loop over every row
+		result += str(item['make']) + '\n'
+
+	return result
+
+if __name__ == "__main__":
+	app.run()

0e54827e9b7592ff349572ce320514f5c1a96bc5
JaneIllario <illariojane@gmail.com>
2019-09-15 05:11:20 -0400

    save all cropped profiles, pulled from data

--- azure_face_detection.py
+++ azure_face_detection.py
@@ -12,43 +12,44 @@ import os
 # Replace <Subscription Key> with your valid subscription key.
 FACE_SUBSCRIPTION_KEY = os.environ['COGNITIVE_SERVICE_KEY']
 
-# Set image path from local file.
-image_path = os.path.join('detection-1-thumbnail.jpg')
+for it, profile_img in enumerate(os.listdir("data")):
+	# Set image path from local file.
+	image_path = os.path.join('data/profile-'+ str(it)+'.jpg')
 
-assert FACE_SUBSCRIPTION_KEY
+	assert FACE_SUBSCRIPTION_KEY
 
-# You must use the same region in your REST call as you used to get your
-# subscription keys. For example, if you got your subscription keys from
-# westus, replace "westcentralus" in the URI below with "westus".
-#
-# Free trial subscription keys are generated in the westcentralus region.
-# If you use a free trial subscription key, you shouldn't need to change
-# this region.
-face_api_url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect'
+	# You must use the same region in your REST call as you used to get your
+	# subscription keys. For example, if you got your subscription keys from
+	# westus, replace "westcentralus" in the URI below with "westus".
+	#
+	# Free trial subscription keys are generated in the westcentralus region.
+	# If you use a free trial subscription key, you shouldn't need to change
+	# this region.
+	face_api_url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-image_data = open(image_path, "rb")
-im = Image.open(image_path)
+	image_data = open(image_path, "rb")
+	im = Image.open(image_path)
 
-headers = {'Content-Type': 'application/octet-stream',
-           'Ocp-Apim-Subscription-Key': FACE_SUBSCRIPTION_KEY}
-params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-    'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-}
+	headers = {'Content-Type': 'application/octet-stream',
+	           'Ocp-Apim-Subscription-Key': FACE_SUBSCRIPTION_KEY}
+	params = {
+	    'returnFaceId': 'true',
+	    'returnFaceLandmarks': 'false',
+	    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+	    'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+	}
 
-response = requests.post(face_api_url, params=params, headers=headers, data=image_data)
-response.raise_for_status()
-faces = response.json()
+	response = requests.post(face_api_url, params=params, headers=headers, data=image_data)
+	response.raise_for_status()
+	faces = response.json()
 
-for it, face in enumerate(faces):
-    fr = face["faceRectangle"]
-    left = fr["left"]
-    top = fr["top"]
-    width = fr["width"]
-    height = fr["height"]
-    cropped_img = im.crop((left, top, (left+width), (top+height)))
-    cropped_img.save("face-" + str(it) + ".jpg", "JPEG")
+	for face in faces:
+		fr = face["faceRectangle"]
+		left = fr["left"]
+		top = fr["top"]
+		width = fr["width"]
+		height = fr["height"]
+		cropped_img = im.crop((left, top, (left+width), (top+height)))
+		cropped_img.save("faces/face-" + str(it) + ".jpg", "JPEG")
 
 print("done")
